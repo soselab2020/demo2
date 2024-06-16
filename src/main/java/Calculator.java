aea35174abd1328815e75fbf2fd8ec5d12fd8e9c
soselab2020 <shangpinma@gmail.com>
2024-06-16 10:16:58 +0800

    Initial commit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,20 @@
+public class Calculator {
+
+    private int status = 0;
+    
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        return (double)a / b;
+    }
+}
